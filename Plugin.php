d1342392f4ca125666763b8cc0ae9ff12db1ded9
Martin <skydiver@users.noreply.github.com>
2018-01-10 12:29:51 -0300

    code linting and cleanup

--- Plugin.php
+++ Plugin.php
@@ -1,63 +1,55 @@
 <?php
 
-    namespace Martin\GitReposManager;
+namespace Martin\GitReposManager;
 
-    use Backend, Controller, Lang, Validator;
-    use System\Classes\PluginBase;
-    use System\Classes\SettingsManager;
+use Backend, Controller, Lang, Validator;
+use System\Classes\PluginBase;
+use System\Classes\SettingsManager;
 
-    class Plugin extends PluginBase {
+class Plugin extends PluginBase {
 
-        public function pluginDetails() {
-            return [
-                'name'        => 'martin.gitreposmanager::lang.plugin.name',
-                'description' => 'martin.gitreposmanager::lang.plugin.description',
-                'author'      => 'Martin',
-                'icon'        => 'icon-cubes'
-            ];
-        }
+    public function pluginDetails() {
+        return [
+            'name'        => 'martin.gitreposmanager::lang.plugin.name',
+            'description' => 'martin.gitreposmanager::lang.plugin.description',
+            'author'      => 'Martin M.',
+            'homepage'    => 'https://github.com/skydiver/',
+            'icon'        => 'icon-cubes'
+        ];
+    }
 
-        public function boot() {
+    public function boot() {
 
-            Validator::extend('isReadable', function($attribute, $value, $parameters, $validator) {
-                if(is_readable($value)) return true;
+        Validator::extend('isReadable', function ($attribute, $value, $parameters, $validator) {
+            if (is_readable($value)) {
+                return true;
+            } else {
                 return false;
-            }, Lang::get('martin.gitreposmanager::lang.model.errors.validator_is_readable'));
-
-        }
-
-        // public function registerNavigation() {
-        //     return [
-        //         'gitreposmanager' => [
-        //             'label'       => 'martin.gitreposmanager::lang.navigation.label',
-        //             'url'         => Backend::url('martin/gitreposmanager/repos'),
-        //             'permissions' => ['martin.gitreposmanager.access_repos'],
-        //             'icon'        => 'icon-cubes',
-        //             'order'       => 500,
-        //         ]
-        //     ];
-        // }
-
-        public function registerSettings() {
-            return [
-                'gitreposmanager' => [
-                    'label'       => 'martin.gitreposmanager::lang.navigation.label',
-                    'description' => 'martin.gitreposmanager::lang.navigation.description',
-                    'category'    => SettingsManager::CATEGORY_SYSTEM,
-                    'icon'        => 'icon-git',
-                    'url'         => Backend::url('martin/gitreposmanager/repos'),
-                    'order'       => 500,
-                    'permissions' => ['martin.gitreposmanager.access_repos'],
-                ]
-            ];
-        }
-
-        public function registerPermissions() {
-            return [
-                'martin.gitreposmanager.access_repos' => ['label' => 'Access Git Repos Manager page', 'tab' => 'Git Repos Manager'],
-            ];
-        }
+            }
+        }, Lang::get('martin.gitreposmanager::lang.model.errors.validator_is_readable'));
 
     }
+
+    public function registerSettings() {
+        return [
+            'gitreposmanager' => [
+                'label'       => 'martin.gitreposmanager::lang.navigation.label',
+                'description' => 'martin.gitreposmanager::lang.navigation.description',
+                'category'    => SettingsManager::CATEGORY_SYSTEM,
+                'icon'        => 'icon-git',
+                'url'         => Backend::url('martin/gitreposmanager/repos'),
+                'order'       => 500,
+                'permissions' => ['martin.gitreposmanager.access_repos'],
+            ]
+        ];
+    }
+
+    public function registerPermissions() {
+        return [
+            'martin.gitreposmanager.access_repos' => ['label' => 'Access Git Repos Manager page', 'tab' => 'Git Repos Manager'],
+        ];
+    }
+
+}
 
 ?>
